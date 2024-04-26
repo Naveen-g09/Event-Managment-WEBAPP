be2ec50a640622d2f1ebaadacb15ed5344e38347
Naveen <naveengummella.rg@gmail.com>
2024-04-26 20:28:34 +0530

    env

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/PrepInstaMERN')
+mongoose.connect('mongodb+srv://naveengummella:<Password>@eventmanagement.5vsjxw9.mongodb.net/')
 .then(() => console.log('Database connected successfully'))
 .catch(() => console.log('Database is not connected'))
